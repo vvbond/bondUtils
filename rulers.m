93448e7ec331f5cea1a1d3d993e31855bd7a9cf6
Vladimir Bondarenko <vladimir.bondarenko@fotechsolutions.com>
2015-05-12 13:27:33 +0100

    Minor changes.

--- rulers.m
+++ rulers.m
@@ -1,7 +1,7 @@
 function rulers(onOffSwitch)
 
 if nargin == 0
-    onOffSwitch = 1;
+    onOffSwitch = 3;
 end
 
 if ~onOffSwitch
@@ -21,23 +21,24 @@ dy = diff(ylims);
 A = [1 2
      1 2]./3;
 lpos = [ xlims(1)+A*dx, ylims(1)+A*dy ]; % lines position [x1 x2 y1 y2].
+lcolor = [.5 .5 .5];
+bgColor = [.8 .8 .8];
  
 hold on;
-hlines = [ plot(lpos(:, 1:2), ylims, 'g--', 'LineWidth', 2, 'Tag', 'rulersLine');
-           plot(xlims, lpos(:, 3:4), 'g--', 'LineWidth', 2, 'Tag', 'rulersLine') ];
+hlines = [ plot(lpos(:, 1:2), ylims, '--', 'LineWidth', 2, 'Color', lcolor, 'Tag', 'rulersLine');
+           plot(xlims, lpos(:, 3:4), '--', 'LineWidth', 2, 'Color', lcolor, 'Tag', 'rulersLine') ];
 
 xlim(xlims);
 ylim(ylims);
 
-sp = '      ';
-sp1 = '  ';
-infoTxt = { ['    x_1', sp, 'x_2', sp 'dx', sp],...
-            [num2str(lpos(1,1)), sp1, num2str(lpos(1,2)), sp1, num2str(abs(diff(lpos(1,1:2)))) ],...
-            ['    y_1', sp, 'y_2', sp, 'dy', sp],...
-            [num2str(lpos(1,3)), sp1, num2str(lpos(1,4)), sp1, num2str(abs(diff(lpos(1,3:4)))) ]};
+infoTxt = { ['    x_1    ', '    x_2    ', '    dx    '],...
+            sprintf('%8.2f %8.2f %8.2f', lpos(1,1), lpos(1,2), abs(diff(lpos(1,1:2)))),...
+            ['    y_1    ', '    y_2    ', '    dy    '],...
+            sprintf('%8.2f %8.2f %8.2f', lpos(1,3), lpos(1,4), abs(diff(lpos(1,3:4))))};
 hinfobx = annotation('textbox', [.13 .1 .1 .1], ...
+                     'FontName', 'Courier',...
                      'string', infoTxt,... 
-                     'Color', 'k', 'BackgroundColor', 'y', 'EdgeColor', 'k', ...
+                     'Color', 'k', 'BackgroundColor', bgColor, 'EdgeColor', 'k', ...
                      'Tag', 'rulersInfo');
 
 %% Set interactions:
@@ -62,12 +63,12 @@ set(gcf, 'WindowButtonMotionFcn', @wbmcb, ...
                     lpos(:,clix) = [1;1]*cxpos;
                     set(hlines(clix), 'XData', lpos(:,clix));
                     % Update info text:
-                    infoTxt{2} = [num2str(lpos(1,1)), sp1, num2str(lpos(1,2)), sp1, num2str(abs(diff(lpos(1,1:2))))];
+                    infoTxt{2} = sprintf('%8.2f %8.2f %8.2f', lpos(1,1), lpos(1,2), abs(diff(lpos(1,1:2))));
                 case 1
                     lpos(:,clix) = [1;1]*cypos;
                     set(hlines(clix), 'YData', lpos(:,clix));
                     % Update info text:
-                    infoTxt{4} = [num2str(lpos(1,3)), sp1, num2str(lpos(1,4)), sp1, num2str(abs(diff(lpos(1,3:4))))];                   
+                    infoTxt{4} = sprintf('%8.2f %8.2f %8.2f', lpos(1,3), lpos(1,4), abs(diff(lpos(1,3:4))));
             end
             % Update info box:
             set(hinfobx, 'String', infoTxt);
