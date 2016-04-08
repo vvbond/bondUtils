eae1c2c3c5c443cc4d132b8241b14c96e5ef6649
Vladimir Bondarenko <vladimir.bondarenko@fotechsolutions.com>
2016-04-08 14:19:39 +0100

    Enhanced roi2 tool to be able to move the ROI. Renamed it to ROI2.

--- ROI2.m
+++ ROI2.m
@@ -1,11 +1,14 @@
-classdef roi2 < handle
+classdef ROI2 < handle
     % Interactive tool for specifying a 2D region of interest (ROI).
     
     %% 23-Apr-2015
     %% (c) Vladimir Bondarenko, http://www.mathworks.co.uk/matlabcentral/fileexchange/authors/52876
     
     properties
-        p           % 2x2 matrix of end points, [p10 p11 p20 p21], of the principle lines.
+        p           % Depends on the mode: 
+                    %  in rectangular ROI mode (mode = 0), p is a  2x2 matrix of [p_tl p_br] points;
+                    %  in parallelogram mode (mode = 1), p is a 4x4 matrix of of end points of 
+                    %  the principle lines, [p11 p12 p21 p22].
         xrng        % ROI x range.
         yrng        % ROI y range.
         x2y         % 2-vector of coefficients, [a; b], of the equation y(x) = a*x+b.
@@ -27,6 +30,8 @@ classdef roi2 < handle
         old_bdcb
         old_bucb
         old_bmcb
+        old_keyPressCb
+        old_keyReleaseCb
         old_fpointer
         old_NextPlot
         axlims
@@ -37,19 +42,37 @@ classdef roi2 < handle
         lineIx      % index of the selected line in the hline array.
         old_cpos    % store cursor position for line movement.
         old_p       % store the ROI points matrix, p.
+        moveROI     % switch between moving and resizing of the ROI.
+    end
+    
+    % Image related properties:
+    properties(Hidden = true)
+        himg        % handle of an image if it exist in the figure.
+        p0          % coordinates of the image origin.
+        dxx         % increment on the x-axis.
+        dyy         % increment on the y-axis.
+        
+    end
+    
+    properties
+        p_ix        % matrix of image indices corresponding to p.
+        xrng_ix     % image indicies corresponding to xrng and
+        yrng_ix     % yrng.
     end
     
     methods
         %% Constructor
-        function xyr = roi2(hfig, roimode)
+        function xyr = ROI2(hfig, roimode)
             
             % Parse input:
             if nargin == 0
                 xyr.hfig  = gcf;
                 xyr.mode = 0;
             elseif nargin == 1 
+                xyr.hfig = hfig;
                 xyr.mode = 0;
             else
+                xyr.hfig = hfig;
                 xyr.mode = roimode;
             end
             
@@ -70,6 +93,29 @@ classdef roi2 < handle
             xyr.y2x = zeros(2,1);
             xyr.type = 0;
             xyr.axlims = axis;
+            
+            % If a figure has an image, grab its handle:
+            xyr.himg = findobj(xyr.hfig, 'type', 'image');
+           
+            % Get scaling info about the image:
+            if ishandle(xyr.himg)
+               xyr.p0 = [xyr.himg.XData(1); xyr.himg.YData(1)];
+
+               [nRows, nCols] = size(xyr.himg.CData);
+               if length(xyr.himg.XData) == nCols
+                   xyr.dxx = diff(xyr.himg.XData(1:2));
+               else
+                   warning('The length of the X-coordinate vector doesn''t match the number of columns in the image.');
+                   xyr.dxx = diff(xyr.himg.XData([1,end]) )/( nCols - 1 );
+               end
+
+               if length(xyr.himg.YData) == nRows
+                   xyr.dyy = diff(xyr.himg.YData(1:2));
+               else
+                   warning('The length of the Y-coordinate vector doesn''t match the number of rows in the image.');
+                   xyr.dyy = diff(xyr.himg.XData([1,end]))/( nRows - 1 );
+               end
+            end
         end
         
         %% Destructor
@@ -77,6 +123,11 @@ classdef roi2 < handle
             
             % Delete the tool's button:
             if ishandle(xyr.hbtn), delete(xyr.hbtn); end
+            % Delete the ROI.
+            if ishandle(xyr.hline), delete(xyr.hline); end
+            % Restore figure keyboard events:
+            set(xyr.hfig, 'KeyPressFcn', xyr.old_keyPressCb, 'KeyReleaseFcn', xyr.old_keyReleaseCb);
+
         end
         
         %% ROI On/Off callbacks
@@ -85,9 +136,13 @@ classdef roi2 < handle
         end
         
         function roiOff(xyr,src,evt)
-            % Delete the previous roi.
+           
+            % Delete the ROI.
             if ishandle(xyr.hline), delete(xyr.hline); end
             xyr.type = 0;
+            
+            % Restore figure keyboard events:
+            set(xyr.hfig, 'KeyPressFcn', xyr.old_keyPressCb, 'KeyReleaseFcn', xyr.old_keyReleaseCb);
         end
         
         %% Create new ROI
@@ -105,6 +160,9 @@ classdef roi2 < handle
             xyr.old_bdcb = get(gcf,'WindowButtonDownFcn');
             xyr.old_bmcb = get(gcf,'WindowButtonMotionFcn');
             xyr.old_bucb = get(gcf,'WindowButtonUpFcn');
+            xyr.old_keyPressCb   = get(gcf, 'KeyPressFcn');
+            xyr.old_keyReleaseCb = get(gcf, 'KeyReleaseFcn');
+
 
             % Clear the mouse event handlers:
             set(xyr.hfig,'WindowButtonDownFcn','');
@@ -120,7 +178,10 @@ classdef roi2 < handle
 
             % Interactions:
             xyr.axlims = axis;
-            set(xyr.hfig, 'windowButtonDownFcn', @(src,evt) roi_bdcb(xyr,src,evt));
+            set(xyr.hfig, 'windowButtonDownFcn', @(src,evt) roi_bdcb(xyr,src,evt), ...
+                          'KeyPressFcn',   @(src,evt) fkpcb(xyr,src,evt), ...
+                          'KeyReleaseFcn', @(src,evt) fkrcb(xyr,src,evt) );
+            xyr.moveROI = 0;
             hold on;
         end
         
@@ -196,14 +257,15 @@ classdef roi2 < handle
                         shiftV = dxdy*[1 1];
                     end
             end
-                % New coordinates:
-                new_p = xyr.old_p(:,pIx) + shiftV;
-                % Check if the ROI exceeds axes limits:
-                if all([new_p(1,:)>=xyr.axlims(1), new_p(1,:)<=xyr.axlims(2),...
-                        new_p(2,:)>=xyr.axlims(3), new_p(2,:)<=xyr.axlims(4)])
-                   xyr.p(:,pIx) = new_p;
-                   xyr.update();
-                end
+            % New coordinates:
+            new_p = xyr.old_p(:,pIx) + shiftV;
+            
+            % Check if the ROI exceeds axes limits:
+            if all([new_p(1,:)>=xyr.axlims(1), new_p(1,:)<=xyr.axlims(2),...
+                    new_p(2,:)>=xyr.axlims(3), new_p(2,:)<=xyr.axlims(4)])
+               xyr.p(:,pIx) = new_p;
+               xyr.update();
+            end
         end
         
         %% Line button up callback
@@ -211,6 +273,75 @@ classdef roi2 < handle
             % Restore figure settings:
             set(xyr.hfig, 'windowButtonMotionFcn', xyr.old_bmcb, ...
                           'windowButtonUpFcn',     xyr.old_bucb );
+        end
+        
+        %% ROI move callbacks
+        function roiMove_bdcb(xyr,src,evt)
+        % Button down callback.
+            cpos = get(gca, 'currentPoint');
+            xyr.old_cpos = cpos(1,1:2)';
+            xyr.old_p = xyr.p;
+            
+            set(xyr.hfig, 'windowButtonMotionFcn', @(src,evt) roiMove_bmcb(xyr,src,evt));
+        end
+        
+        function roiMove_bucb(xyr,src,evt)
+        % Button up callback.
+            set(xyr.hfig, 'windowButtonMotionFcn', xyr.old_bmcb);
+        end
+        
+        function roiMove_bmcb(xyr,src,evt)
+        % Button motion callback.
+            cpos = get(gca, 'currentPoint');
+            cpos = cpos(1,1:2)';
+            shiftV = cpos - xyr.old_cpos;     % shift vector.
+            
+            % New coordinates:
+            v1 = ones(1, size(xyr.p, 2));
+            new_p = xyr.old_p + shiftV*v1;
+            
+            % Check if the ROI exceeds axes limits:
+            if all([new_p(1,:)>=xyr.axlims(1), new_p(1,:)<=xyr.axlims(2),...
+                    new_p(2,:)>=xyr.axlims(3), new_p(2,:)<=xyr.axlims(4)])
+               xyr.p = new_p;
+               xyr.update();
+            end
+        end
+        
+        %% Keyboard events callbacks
+        function fkpcb(xyr, src, evt)
+        % Handler for the figure keypress event.
+            if strcmpi(evt.Modifier,'control')
+                cpos = get(gca, 'currentPoint');
+                cpos = cpos(1,1:2)';
+                
+                % If the pointer is within the ROI:
+                if all(prod([cpos cpos] - [xyr.xrng; xyr.yrng], 2) < 0) 
+                    xyr.moveROI = 1;
+                    set(gcf,'Pointer','fleur');
+                    
+                    % Store previous interaction callbacks:
+                    xyr.old_bdcb = get(gcf,'WindowButtonDownFcn');
+                    xyr.old_bucb = get(gcf,'WindowButtonUpFcn');
+                    xyr.old_bmcb = get(gcf,'WindowButtonMotionFcn');
+                    
+                    % Set new interaction callbacks:
+                    set(xyr.hfig, 'windowButtonDownFcn', @(src,evt) roiMove_bdcb(xyr,src,evt), ...
+                                  'windowButtonUpFcn', @(src,evt) roiMove_bucb(xyr,src,evt) );
+
+                end
+            end
+        end
+
+        function fkrcb(xyr,src,evt)
+        % Handler for the figure keyboard release event.
+            xyr.moveROI = 0;
+            set(gcf,'Pointer','arrow');
+            
+            % Restore callbacks:
+            set(xyr.hfig, 'windowButtonMotionFcn', xyr.old_bmcb, ...
+                          'windowButtonUpFcn',     xyr.old_bucb, ...
+                          'windowButtonDownFcn',   xyr.old_bdcb);
         end
         
         %% Update ROI
@@ -223,15 +354,20 @@ classdef roi2 < handle
                     set(xyr.hline(2), 'xdata', xyr.p(1,1:2), 'ydata', xyr.p(2,[2 2]));
                     set(xyr.hline(3), 'xdata', xyr.p(1,[1 1]), 'ydata', xyr.p(2,1:2));
                     set(xyr.hline(4), 'xdata', xyr.p(1,[2 2]), 'ydata', xyr.p(2,1:2));
+                    
+                    % Update ranges:
+                    xyr.xrng = [min(xyr.p(1,:)) max(xyr.p(1,:))];
+                    xyr.yrng = [min(xyr.p(2,:)) max(xyr.p(2,:))];
                 case 1
                     set(xyr.hline(1), 'xdata', xyr.p(1,[1 2]), 'ydata', xyr.p(2,[1 2]));
                     set(xyr.hline(2), 'xdata', xyr.p(1,[3 4]), 'ydata', xyr.p(2,[3 4]));
                     set(xyr.hline(3), 'xdata', xyr.p(1,[1 3]), 'ydata', xyr.p(2,[1 3]));
                     set(xyr.hline(4), 'xdata', xyr.p(1,[2 4]), 'ydata', xyr.p(2,[2 4]));
 
-                    % Update equations:
+                    % Update ranges:
                     xyr.xrng = [min(xyr.p(1,:)) max(xyr.p(1,:))];
                     xyr.yrng = [min(xyr.p(2,:)) max(xyr.p(2,:))];
+                    
                     % Coefficients of the y = a*x+b line:
                     xyr.x2y = [ diff(xyr.p(2,[1 2]));
                                -det(xyr.p(:,[1 2]))  ]/diff(xyr.p(1,[1 2]));
@@ -272,6 +408,18 @@ classdef roi2 < handle
                 set(xyr.hfig, 'windowButtonMotionFcn', @(src,evt) roi_bmcb(xyr,src,evt) );
             else
                 xyr.clicks = 0;
+                
+                % Compute the x- and y- ranges:
+                xyr.xrng = [min(xyr.p(1,:)) max(xyr.p(1,:))];
+                xyr.yrng = [min(xyr.p(2,:)) max(xyr.p(2,:))];
+                
+                % Find corresponding image indices:
+                if ishandle(xyr.himg)
+                    v1 = ones(1, size(xyr.p,2));
+                    xyr.p_ix = round(xyr.p - xyr.p0*v1)./([xyr.dxx; xyr.dyy]*v1)+1;
+                    xyr.xrng_ix = round(xyr.xrng - xyr.p0(1)*v1)./[xyr.dxx xyr.dxx]+1;
+                    xyr.yrng_ix = round(xyr.yrng - xyr.p0(2)*v1)./[xyr.dyy xyr.dyy]+1;
+                end
 
                 % Restore figure settings:
                 set(xyr.hfig, 'windowButtonMotionFcn', xyr.old_bmcb );
@@ -279,10 +427,6 @@ classdef roi2 < handle
                 set(xyr.hfig, 'windowButtonDownFcn',   xyr.old_bdcb );
                 set(xyr.hfig, 'pointer', xyr.old_fpointer);
                 set(gca, 'NextPlot', xyr.old_NextPlot);
-
-                % Compute the x- and y- ranges:
-                xyr.xrng = [min(xyr.p(1,:)) max(xyr.p(1,:))];
-                xyr.yrng = [min(xyr.p(2,:)) max(xyr.p(2,:))];
 
                 % Activate interaction:
                 set(xyr.hline, 'buttonDownFcn', @(src,evt) line_bdcb(xyr,src,evt));
@@ -404,6 +548,34 @@ classdef roi2 < handle
                 case 3
                     disp('hey, you shouldn''t ever see this message.');
             end            
+        end
+        
+        
+        %% Sample ROI
+        function [D, xroi, yroi] = sample(xyr)
+            
+            % Sanity check:
+            if ~ishandle(xyr.himg)
+                warning('Image not found.')
+                return;
+            end
+            
+            switch xyr.mode
+                case 0      % Rectangular ROI => simple extraction.
+                    x_ix = xyr.xrng_ix(1):xyr.xrng_ix(2);
+                    y_ix = xyr.yrng_ix(1):xyr.yrng_ix(2);
+                    D = xyr.himg.CData(y_ix, x_ix);
+                    if nargout > 1
+                        try
+                            xroi = xyr.himg.XData(x_ix);
+                            yroi = xyr.himg.YData(y_ix);
+                        catch M
+                            warning(['Couldn''t extract axes coordinates. ' M.message]);
+                            xroi = []; yroi = [];
+                        end
+                    end
+                case 1
+            end
         end
     end
 end
