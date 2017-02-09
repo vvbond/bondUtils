cebd3a61f606df609d3c794406d4e46f25c18af5
Vladimir Bondarenko <vladimir.bondarenko@fotechsolutions.com>
2017-02-09 13:04:07 +0000

    Added linkAllAxes.m: link the axes in all open figures.

---
+++ linkAllAxes.m
@@ -0,0 +1,31 @@
+function linkTheAxes(axName)
+% Link all the axes in figures.
+%
+% Usage: linkTheAxes(axName)
+%
+% INPUT:
+% axName - string, 'x', 'y', 'xy' (default), or 'off'. Indicates which axis to link.
+%
+% OUTPUT:
+%  None.
+%
+% Examples:
+%  linkTheAxes(axName);
+%
+% See also: linkaxes, gaa, delfigs.
+ 
+%% Created: 18-Mar-2016 17:45:29
+%% (c) Vladimir Bondarenko, http://www.mathworks.co.uk/matlabcentral/fileexchange/authors/52876    
+
+    % Parse input:
+    if nargin == 0, axName = 'xy'; end
+    
+    if ~any( strcmpi(axName, {'x', 'y', 'xy', 'off'}) )
+        error('Wrong axis name.');
+    end
+    
+    % Main:
+    axs = gaa;
+    
+    linkaxes(axs, axName);
+end
