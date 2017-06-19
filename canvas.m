8fdb481c7102f0d002aef2b64b162c70db2baf6f
Vladimir Bondarenko <vladimir.bondarenko@fotechsolutions.com>
2017-06-19 17:01:10 +0100

    canvas: added y scale argument.

--- canvas.m
+++ canvas.m
@@ -1,10 +1,19 @@
 function canvas(xscale, yscale)
+% Convenience function for axes setup.
+
+    % Parse input:
     switch nargin
         case 0
-            xscale = 10; yscale = 10; 
+            xscale = [0 10]; yscale = [0 10]; 
         case 1
             yscale = xscale;
     end
-    axis([0 xscale 0 yscale]);
+    
+    % Augment scalar scale values:
+    if numel(xscale) == 1, xscale = [0 xscale]; end
+    if numel(yscale) == 1, yscale = [0 yscale]; end
+    
+    % Setup axes:
+    axis([xscale yscale]);
     grid on; box on
 end
