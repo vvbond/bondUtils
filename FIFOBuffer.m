81932acef2b8bf35b0e5b101cef993550b348671
Vladimir Bondarenko <vladimir.bondarenko@fotechsolutions.com>
2017-07-04 12:13:55 +0100

    FIFOBuffer: documentation.

--- FIFOBuffer.m
+++ FIFOBuffer.m
@@ -1,13 +1,29 @@
 classdef FIFOBuffer < handle
+% Class implementing a fixed-size FIFO buffer for arrays.
+%
+% Usage: 
+%  bfr = FIFOBuffer(elsz, numels);  % Create a buffer.
+%  bfr.push(el);                    % Push an element to the buffer.
+%  D = bfr.data;                    % Fetch buffer data.    
+%
+% Examples:
+%   bfr = FIFOBuffer([1 1], 10); for ii=1:13, bfr.push(rand); disp(bfr.data); end
+
     properties
-        elsz
-        numels
-        Q
-        qix
-        push_count
-        elix
+        elsz                % {2,3}-vector describing the size of indivudual elements in the buffer.
+        numels              % scalar, number of elements in the buffer.
     end
+    
+    properties(Hidden)
+        Q                   % buffer que: an array of structures, Q.A, where A holds data of a single element.
+        qix                 % rotating que index.
+        elix                % index for the next element to be pushed to the Q.
+        push_count          % push counter (unused, 'just in case' variable).
+        
+    end
+    
     methods
+        %% {Con,De}structor
         function bfr = FIFOBuffer(elsz, numels)
             bfr.numels = numels;
             bfr.elsz = elsz;
@@ -18,22 +34,25 @@ classdef FIFOBuffer < handle
             bfr.qix = 1:bfr.numels;
             bfr.elix = 1;
             bfr.push_count = 0;
-            D = zeros(bfr.elsz.*[1 bfr.numels]);
         end
         
+        %% Push to buffer
         function push(bfr, el)
+        % Push an element to the buffer.
             
             % Consistency check:
             if ~all(size(el) == bfr.elsz)
                 error('Wrong element size.');
             end
             
-            bfr.push_count = bfr.push_count + 1;
             bfr.Q(bfr.elix).A = el;
             bfr.elix = mod(bfr.elix, bfr.numels) + 1;
             bfr.qix = circshift(bfr.qix, -1);
+            
+            bfr.push_count = bfr.push_count + 1;    
         end
         
+        %% Fetch data
         function D = data(bfr)
             D = [bfr.Q(bfr.qix).A];
         end        
