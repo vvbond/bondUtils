3a2ba12e76f86523676f9d92828f300f4bb4667a
Vladimir Bondarenko <vladimir.bondarenko@fotechsolutions.com>
2017-10-09 18:02:19 +0100

    numhandle: wrapper handle class to numerica data to be able to pass data by reference.

---
+++ numhandle.m
@@ -0,0 +1,166 @@
+classdef numhandle < handle
+    properties
+        data
+    end
+    %% {Con,De}structor
+    methods
+        function nth = num2handle(num)
+            nth.data = num;
+        end
+    end
+    %% Setter
+    methods
+        function set(nth, val)
+            nth.data = val;
+        end
+    end
+    %% Operators overloading
+    methods
+        function d = double(nth)
+            d = nth.data;
+        end
+        
+        function r = plus(obj1, obj2)
+            a = double(obj1);
+            b = double(obj2);
+            r = a + b;
+        end
+        
+        function r = minus(obj1, obj2)
+            a = double(obj1);
+            b = double(obj2);
+            r = a - b;
+        end
+        
+        function r = uminus(obj1)
+            a = double(obj1);            
+            r = -a;
+        end
+        
+        function r = uplus(obj1)
+            a = double(obj1);            
+            r = +a;
+        end
+                
+        function r = times(obj1, obj2)
+            a = double(obj1);
+            b = double(obj2);
+            r = a .* b;
+        end
+        
+        function r = mtimes(obj1, obj2)
+            a = double(obj1);
+            b = double(obj2);
+            r = a * b;
+        end
+        
+        function r = rdivide(obj1, obj2)
+            a = double(obj1);
+            b = double(obj2);
+            r = a ./ b;
+        end
+
+        function r = ldivide(obj1, obj2)
+            a = double(obj1);
+            b = double(obj2);
+            r = a .\ b;
+        end
+        
+        function r = mrdivide(obj1, obj2)
+            a = double(obj1);
+            b = double(obj2);
+            r = a / b;
+        end
+
+        function r = mldivide(obj1, obj2)
+            a = double(obj1);
+            b = double(obj2);
+            r = a \ b;
+        end
+        
+        function r = power(obj1, obj2)
+            a = double(obj1);
+            b = double(obj2);
+            r = a.^b;
+        end
+
+        function r = mpower(obj1, obj2)
+            a = double(obj1);
+            b = double(obj2);
+            r = a^b;
+        end
+        
+        function r = eq(obj1, obj2)
+            a = double(obj1);
+            b = double(obj2);
+            r = a == b;
+        end
+        
+        function r = ne(obj1, obj2)
+            a = double(obj1);
+            b = double(obj2);
+            r = a ~= b;
+        end
+
+        
+        function r = lt(obj1, obj2)
+            a = double(obj1);
+            b = double(obj2);
+            r = a < b;
+        end        
+
+        function r = gt(obj1, obj2)
+            a = double(obj1);
+            b = double(obj2);
+            r = a > b;
+        end        
+
+        function r = le(obj1, obj2)
+            a = double(obj1);
+            b = double(obj2);
+            r = a <= b;
+        end        
+
+        function r = ge(obj1, obj2)
+            a = double(obj1);
+            b = double(obj2);
+            r = a >= b;
+        end
+
+        function r = and(obj1, obj2)
+            a = double(obj1);
+            b = double(obj2);
+            r = a & b;
+        end
+
+        function r = or(obj1, obj2)
+            a = double(obj1);
+            b = double(obj2);
+            r = a | b;
+        end
+
+        function r = not(obj1)
+            a = double(obj1);            
+            r = a.';
+        end
+        
+        function r = trnspose(obj1)
+            a = double(obj1);            
+            r = ~a;
+        end
+        
+        function r = ctrnspose(obj1)
+            a = double(obj1);            
+            r = a';
+        end
+        
+        function varargout = subsref(obj1, s)
+            if strcmp(s(1).type, '.')
+                builtin('subsref', obj1, s);
+            else
+                a = double(obj1);
+                varargout = {builtin('subsref', a, s)};
+            end
+        end
+    end
+end
