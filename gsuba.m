53d79d7cbabf48aa9a1eb1b0cf23d2c64e553dd5
Vladimir Bondarenko <vladimir.bondarenko@fotechsolutions.com>
2017-11-07 10:25:43 +0000

    gsuba() accepts figure handle as input.

--- gsuba.m
+++ gsuba.m
@@ -1,21 +1,25 @@
-function hax = gsuba()
-% Get subaxes in the current figure.
+function hax = gsuba(hfig)
+% Get subaxes handles in a given figure.
 %
 % Usage: hax = gsuba()
-%
+%        hax = gsuba(hfig)
 % INPUT:
-%  None.
+% Optional:
+%  hfig     - figure handle. If omitted, the current figure is used.
 %
 % OUTPUT:
-% hax - Array of subaxes handles.
+%  hax      - Array of subaxes handles.
 %
 % Examples:
 %  gsuba();
-%  [hax]=gsuba();
+%  hax = gsuba();
 %
 % See also: gaa, linkTheAxes.
  
 %% Created: 19-Jun-2017 12:07:23
 %% (c) Vladimir Bondarenko, http://www.mathworks.co.uk/matlabcentral/fileexchange/authors/52876
 
-hax = findall(get(gcf,'Children'), 'Type', 'Axes');
+if nargin == 0
+    hfig = gcf;
+end
+hax = findall(get(hfig,'Children'), 'Type', 'Axes');
