af7ef1cbc2360c4e0f15d37ddbc9af1b28b4c2c0
Vladimir Bondarenko <vladimir.bondarenko@fotechsolutions.com>
2017-11-30 15:26:13 +0000

    CircBufferT: made more consistent with the CircBuffer class.

--- CircBufferT.m
+++ CircBufferT.m
@@ -1,4 +1,4 @@
-classdef CircBufferT < handle
+classdef CircBufferT < Sink
 % Circular buffer for typed data.
 %
 % Usage: 
@@ -11,49 +11,76 @@ classdef CircBufferT < handle
 
     properties
         Q                   % buffer que: an array of structures, Q.el, where 'el' holds data of a single element.        
-        elsz                % {2,3}-vector describing the size of indivudual elements in the buffer.
-        numels              % scalar, number of elements in the buffer.
+        width               % scalar, number of elements in the buffer.
+        cursor              % index for the next element to be pushed to the Q.
+        count               % push counter (unused, 'just in case' variable).
+        delay = 0
+    end
+    properties(Dependent)
+        ix                  % rotating que index.
     end
     
-    properties(Hidden)
-        ix                 % rotating que index.
-        elix                % index for the next element to be pushed to the Q.
-        count               % push counter (unused, 'just in case' variable).        
+    events
+        initialized
     end
-    
+
     methods
         %% {Con,De}structor
-        function bfr = CircBufferT(numels)
-            bfr.numels = numels;
+        function bfr = CircBufferT(width)
+            bfr.width = width;
+            
+            bfr.init();
+            bfr.marker = 'd';
+        end
+        
+        function init(bfr)
 
-            % Init:
-            for ii=1:bfr.numels
+            for ii=1:bfr.width
                 bfr.Q(ii).el = [];
             end
-            bfr.qix = 1:bfr.numels;
-            bfr.elix = 1;
-            bfr.push_count = 0;
+            
+            bfr.cursor = 0;
+            bfr.count = 0;
+            % Let connected readers know:
+            notify(bfr, 'initialized');
         end
         
         %% Push to buffer
         function push(bfr, el)
         % Push an element to the buffer.
             
+%             n = length(el);
+            n = 1;
             % Consistency check:
-            if ~all(size(el) == bfr.elsz)
-                error('Wrong element size.');
-            end
+%             if ~all(size(el) == bfr.elsz)
+%                 error('Wrong element size.');
+%             end
             
-            bfr.Q(bfr.elix).A = el;
-            bfr.elix = mod(bfr.elix, bfr.numels) + 1;
-            bfr.qix = circshift(bfr.qix, -1);
-            
-            bfr.push_count = bfr.push_count + 1;    
+            ix_linear = bfr.cursor + (1:n);
+            ix_wrap   = mod(ix_linear-1, bfr.width)+1;
+            bfr.Q(ix_wrap).el = el;            
+            bfr.cursor = ix_wrap(end);
+            bfr.count = bfr.count + n;
         end
         
         %% Fetch data
-        function D = data(bfr)
-            D = [bfr.Q(bfr.qix).A];
+        function D = data(bfr, dIx)
+            if nargin == 1
+                dIx = bfr.ix;
+            end
+            D = [bfr.Q(dIx).el];
         end        
+        
+        %% Setters/Getters
+        function val = get.ix(bfr)
+            ix_linear = bfr.cursor+(1:bfr.width);
+            ix_wrap = mod(ix_linear-1, bfr.width)+1;
+            val = ix_wrap;
+        end
+        
+        %% Wrappers
+        function write(bfr, A)
+            bfr.push(A);
+        end
     end
 end
