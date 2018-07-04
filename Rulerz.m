ce286fbff4e3b4cc8283d09932ab8799f1923ce3
Vladimir Bondarenko <vladimir.bondarenko@fotechsolutions.com>
2018-07-04 10:26:12 +0100

    Rulerz: added rulers move functionality.

--- Rulerz.m
+++ Rulerz.m
@@ -22,8 +22,6 @@ classdef Rulerz < iTool
         hBtn        % handle of the toggle button.
         hLines      % handles of the ruler lines.
         hInfoBox    % handle of the annotation text box.
-        oldButtonMotionFcn
-        oldButtonUpFcn
     end
     
     events
@@ -52,20 +50,22 @@ classdef Rulerz < iTool
                     rlzIcon = load(fullfile(fileparts(mfilename('fullpath')),'/icons/Rulerz.mat'));
             end
 
-            rlz.hBtn = uitoggletool(ht(1), 'OnCallback', @(src,evt) RulerzON(rlz,src,evt),...
-                                        'OffCallback',@(src,evt) RulerzOFF(rlz,src,evt),...
-                                        'CData', rlzIcon.cdata, ...
-                                        'TooltipString', 'X-/Y- rulers', ...
-                                        'Tag', 'rulerz ',... 
-                                        'Separator', 'on');
+            rlz.hBtn = uitoggletool(ht(1),  'OnCallback', @(src,evt) RulerzON(rlz,src,evt),...
+                                            'OffCallback',@(src,evt) RulerzOFF(rlz,src,evt),...
+                                            'CData', rlzIcon.cdata, ...
+                                            'TooltipString', 'X-/Y- rulers', ...
+                                            'Tag', 'rulerz',... 
+                                            'Separator', 'on');
             rlz.lColor = [.5 .5 .5];
             rlz.lStyle = '--';
             rlz.lWidth = 2;
             rlz.annotationBgColor = [.95 .95 .95];
             rlz.annotationPosition = [0 .1 .1 .1];
 
-            % Add listener:
+            % Add listeners:
             addlistener(rlz, 'axisChange', @(src, evt) axisChangeEvt(rlz,src,evt));
+            addlistener(rlz, 'xx', 'PostSet', @(src, evt) xx_PostSet_cb(rlz,src,evt));
+            addlistener(rlz, 'yy', 'PostSet', @(src, evt) yy_PostSet_cb(rlz,src,evt));
         end
         
         %% Destructor
@@ -91,9 +91,10 @@ classdef Rulerz < iTool
         function RulerzON(rlz, ~, ~)
         % Display ruler lines.
             
+            rlz.hax = gca;
             % Define x- and y- coordinates of the ruler lines:            
-            xlims = get(gca,'xlim')';
-            ylims = get(gca,'ylim')';
+            xlims = get(rlz.hax,'xlim')';
+            ylims = get(rlz.hax,'ylim')';
 
             dx = diff(xlims);
             dy = diff(ylims);
@@ -112,8 +113,8 @@ classdef Rulerz < iTool
                 case {2, 'y'}
                     rlz.hLines = plot(xlims, [1;1]*rlz.yy, rlz.lStyle, 'LineWidth', rlz.lWidth, 'Color', rlz.lColor, 'Tag', 'rulersLine');
                 otherwise 
-                    rlz.hLines = [ plot([1;1]*rlz.xx, ylims, rlz.lStyle, 'LineWidth', rlz.lWidth, 'Color', rlz.lColor, 'Tag', 'rulersLine')
-                                   plot(xlims, [1;1]*rlz.yy, rlz.lStyle, 'LineWidth', rlz.lWidth, 'Color', rlz.lColor, 'Tag', 'rulersLine') ];
+                    rlz.hLines = [  plot([1;1]*rlz.xx, ylims, rlz.lStyle, 'LineWidth', rlz.lWidth, 'Color', rlz.lColor, 'Tag', 'rulersLine'),...
+                                    plot(xlims, [1;1]*rlz.yy, rlz.lStyle, 'LineWidth', rlz.lWidth, 'Color', rlz.lColor, 'Tag', 'rulersLine') ];
             end
             xlim(xlims);
             ylim(ylims);
@@ -128,15 +129,22 @@ classdef Rulerz < iTool
                                       'EdgeColor', 'k', ...
                                       'Tag', 'rulersInfo');            
             
+            % Store current axis buttond down function:
+            rlz.cache.abdcb = get(rlz.hax, 'ButtonDownFcn');
+                                  
             % Set lines interaction callback:
             rlz.interactivesOff(rlz.hfig);
             set(rlz.hLines, 'ButtonDownFcn', @(src,evt) lbdcb(rlz, src, evt));
+            set(rlz.hax,    'ButtonDownFcn', @(src,evt) abdcb(rlz, src, evt));
         end
         
         %% Rulerz OFF:
-        function RulerzOFF(~, ~, ~)
+        function RulerzOFF(rlz, ~, ~)
             delete(findall(gcf, 'tag', 'rulersLine'));
             delete(findall(gcf, 'tag', 'rulersInfo'));
+            
+            % Restore callbacks:
+            set(rlz.hax, 'ButtonDownFcn', rlz.cache.abdcb);
         end
         
         %% Interaction callbacks:
@@ -147,18 +155,39 @@ classdef Rulerz < iTool
             rlz.lineIx = find(src == rlz.hLines); % line index.
             
             % Store old interaction callbacks:
-            rlz.oldButtonMotionFcn = get(gcf, 'WindowButtonMotionFcn');
-            rlz.oldButtonUpFcn = get(gcf, 'WindowButtonUpFcn');
+            rlz.cache.windowButtonMotionFcn = get(gcf, 'WindowButtonMotionFcn');
+            rlz.cache.windowButtonUpFcn     = get(gcf, 'WindowButtonUpFcn');
+            rlz.cache.pointer               = get(gcf, 'Pointer');
             
             % Set new interaction callbacks:
-            set(gcf, 'WindowButtonMotionFcn', @(src,evt) wbmcb(rlz, src, evt), ...
-                     'WindowButtonUpFcn', @(src, evt) wbucb(rlz, src, evt));
+            set(gcf, 'WindowButtonMotionFcn', @(src,evt)  wbmcb(rlz, src, evt), ...
+                     'WindowButtonUpFcn',     @(src, evt) wbucb(rlz, src, evt));
+            switch(rlz.lineIx)
+                case 1
+                    switch rlz.axis
+                        case {2, 'y'}
+                            set(gcf, 'Pointer', 'bottom');
+                        otherwise
+                            set(gcf, 'Pointer', 'left');
+                    end
+                case 2
+                    switch rlz.axis
+                        case {2, 'y'}
+                            set(gcf, 'Pointer', 'top');
+                        otherwise
+                            set(gcf, 'Pointer', 'right');
+                    end                    
+                case 3
+                    set(gcf, 'Pointer', 'bottom');
+                case 4
+                    set(gcf, 'Pointer', 'top');                    
+            end
         end
-
+        
         function wbmcb(rlz, ~, ~)
         % Window button motion callback.
             if rlz.lineIx
-                cpos = get(gca, 'CurrentPoint');
+                cpos = get(rlz.hax, 'CurrentPoint');
                 cxpos = cpos(1,1); % cursor x position.
                 cypos = cpos(1,2); % cursor x position.
                 
@@ -167,11 +196,11 @@ classdef Rulerz < iTool
                 switch find(diff(xydata)==0)
                     case 1
                         rlz.xx(rlz.lineIx) = cxpos;
-                        set(rlz.hLines(rlz.lineIx), 'XData', [1 1]*rlz.xx(rlz.lineIx));
+%                         set(rlz.hLines(rlz.lineIx), 'XData', [1 1]*rlz.xx(rlz.lineIx));
                     case 2
                         ix = rlz.lineIx-(rlz.lineIx>2)*2;
                         rlz.yy(ix) = cypos;
-                        set(rlz.hLines(rlz.lineIx), 'YData', [1 1]*rlz.yy(ix));
+%                         set(rlz.hLines(rlz.lineIx), 'YData', [1 1]*rlz.yy(ix));
                 end
                 % Update info box:
                 set(rlz.hInfoBox, 'String', rlz.infoString() );
@@ -180,6 +209,10 @@ classdef Rulerz < iTool
                 for ii=1:length(rlz.bmfun)
                     rlz.bmfun{ii}(rlz);
                 end
+            else
+                dp = rlz.currentPoint - rlz.clickPoint;
+                rlz.xx = rlz.cache.xx + dp(1);
+                rlz.yy = rlz.cache.yy + dp(2);
             end
         end
 
@@ -187,9 +220,28 @@ classdef Rulerz < iTool
         % Window button up callback.
             rlz.lineIx = 0;
             % Restore the old interaction callbacks:
-            set(gcf, 'WindowButtonMotionFcn', rlz.oldButtonMotionFcn,...
-                     'WindowButtonUpFcn',     rlz.oldButtonUpFcn);
+            set(gcf, 'WindowButtonMotionFcn', rlz.cache.windowButtonMotionFcn,...
+                     'WindowButtonUpFcn',     rlz.cache.windowButtonUpFcn,...
+                     'Pointer',               rlz.cache.pointer);
         end
+        
+        function abdcb(rlz, ~, ~)
+            rlz.clickPoint = rlz.currentPoint;
+            rlz.cache.xx = rlz.xx;
+            rlz.cache.yy = rlz.yy;
+            rlz.lineIx = 0;
+            if inarange(rlz.clickPoint(1), rlz.xx) || inarange(rlz.clickPoint(2), rlz.yy)
+                % Store old interaction callbacks:
+                rlz.cache.windowButtonMotionFcn = get(gcf, 'WindowButtonMotionFcn');
+                rlz.cache.windowButtonUpFcn     = get(gcf, 'WindowButtonUpFcn');
+                rlz.cache.pointer               = get(gcf, 'Pointer');
+                
+                % Set interaction functions:
+                set(gcf, 'windowButtonMotionFcn', @(src,evt) wbmcb(rlz,src,evt),...
+                         'windowButtonUpFcn',     @(src,evt) wbucb(rlz,src,evt),...
+                         'pointer', 'fleur');
+            end
+        end        
         
         %% Info string:
         function s = infoString(rlz)
@@ -209,8 +261,7 @@ classdef Rulerz < iTool
         end
         
         %% Listener callback for the axisChange event
-        function axisChangeEvt(rlz, ~, ~)
-    
+        function axisChangeEvt(rlz, ~, ~)   
             % Turn off the current ruler:
             if ishandle(rlz.hBtn)
                 set(rlz.hBtn, 'state', 'off');
@@ -225,6 +276,37 @@ classdef Rulerz < iTool
                 end
                 set(rlz.hBtn, 'CData', rlzIcon.cdata);
             end
+        end
+        
+        function xx_PostSet_cb(rlz, ~, ~)
+            if isempty(rlz.hLines) || strcmpi(rlz.axis, 'y'), return; end
+            
+            if ishandle(rlz.hLines(1)) && isvalid(rlz.hLines(1))
+                set(rlz.hLines(1), 'XData', [1 1]*rlz.xx(1));
+            end
+            if ishandle(rlz.hLines(2)) && isvalid(rlz.hLines(2))
+                set(rlz.hLines(2), 'XData', [1 1]*rlz.xx(2)); 
+            end 
+        end
+
+        function yy_PostSet_cb(rlz, ~, ~)
+            if isempty(rlz.hLines), return; end
+            
+            if strcmpi(rlz.axis, 'y')
+                lIx = 1;
+            elseif numel(rlz.hLines) > 2
+                lIx = 3;
+            else
+                lIx = 0;
+            end
+            
+            if lIx
+                for kk = 0:1
+                    if ishandle(rlz.hLines(lIx+kk)) && isvalid(rlz.hLines(lIx+kk))
+                        set(rlz.hLines(lIx+kk), 'YData', [1 1]*rlz.yy(kk+1));
+                    end
+                end
+            end            
         end
     end    
 end
