d85853c144f026d79564c6e999a118b7bb4c1b7f
Vladimir Bondarenko <vbondarenko@>
2022-01-21 15:45:56 +0000

    dirrec consistency fix: introduced 'data' field to all elements in the output struct array.

--- dirrec.m
+++ dirrec.m
@@ -16,20 +16,18 @@ function ds = dirrec(dirname)
 % See also: <other funame>.
  
 %% Created: 14-Dec-2021 18:25:54
-%% (c) Vladimir Bondarenko, Albus Health
+%% (c) Vladimir Bondarenko
 
 ds = dir(dirname);
 ds(contains({ds.name}, regexpPattern('^\.'))) = [];
+if isempty(ds), return; end
+ds(1).data = [];
 
 subdir_ind = [ds.isdir];
-if isempty(subdir_ind)
-    return; 
-end
+if isempty(subdir_ind), return; end
 
 for ix = find(subdir_ind)
     name = ds(ix).name;
     folder = ds(ix).folder;
     ds(ix).data = dirrec(fullfile(folder, name));
 end
-
-
